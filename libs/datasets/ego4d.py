2d62683f27786703ef8751dab927a8a61f711d96
Lorenzo De Marco <lorenzo.demarco002@gmail.com>
2026-04-20 21:17:34 +0200

    fix

--- libs/datasets/ego4d.py
+++ libs/datasets/ego4d.py
@@ -1,6 +1,7 @@
 import os
 import json
 import numpy as np
+import glob # Aggiunto per cercare i file con nomi lunghi
 
 import torch
 from torch.utils.data import Dataset
@@ -13,43 +14,34 @@ from .data_utils import truncate_feats
 class EGO4DDataset(Dataset):
     def __init__(
         self,
-        is_training,     # if in training mode
-        split,           # split, a tuple/list allowing concat of subsets
-        feat_folder,     # folder for features
-        json_file,       # json file for annotations
-        feat_stride,     # temporal stride of the feats
-        num_frames,      # number of frames for each feat
-        default_fps,     # default fps
-        downsample_rate, # downsample rate for feats
-        max_seq_len,     # maximum sequence length during training
-        trunc_thresh,    # threshold for truncate an action segment
-        crop_ratio,      # a tuple (e.g., (0.9, 1.0)) for random cropping
-        input_dim,       # input feat dim
-        num_classes,     # number of action categories
-        file_prefix,     # feature file prefix if any
-        file_ext,        # feature file extension if any
-        force_upsampling # force to upsample to max_seq_len
+        is_training,     
+        split,           
+        feat_folder,     
+        json_file,       
+        feat_stride,     
+        num_frames,      
+        default_fps,     
+        downsample_rate, 
+        max_seq_len,     
+        trunc_thresh,    
+        crop_ratio,      
+        input_dim,       
+        num_classes,     
+        file_prefix,     
+        file_ext,        
+        force_upsampling 
     ):
-        # file path
         if not isinstance(feat_folder, (list, tuple)):
             feat_folder = (feat_folder, )
-        assert all([os.path.exists(folder) for folder in feat_folder])
-        assert os.path.exists(json_file)
-        assert isinstance(split, tuple) or isinstance(split, list)
-        assert crop_ratio == None or len(crop_ratio) == 2
+
         self.feat_folder = feat_folder
-        if file_prefix is not None:
-            self.file_prefix = file_prefix
-        else:
-            self.file_prefix = ''
+        self.file_prefix = file_prefix if file_prefix is not None else ''
         self.file_ext = file_ext
         self.json_file = json_file
 
-        # split / training mode
-        self.split = split
+        self.split = [s.lower() for s in split] 
         self.is_training = is_training
 
-        # features meta info
         self.feat_stride = feat_stride
         self.num_frames = num_frames
         self.input_dim = input_dim
@@ -61,65 +53,58 @@ class EGO4DDataset(Dataset):
         self.label_dict = None
         self.crop_ratio = crop_ratio
 
-        # load database and select the subset
         dict_db, label_dict = self._load_json_db(self.json_file)
-        assert len(label_dict) == num_classes
+        
+        if len(label_dict) != num_classes:
+            print(f"--> INFO: Updating num_classes from {num_classes} to {len(label_dict)}")
+            self.num_classes = len(label_dict)
+            
         self.data_list = dict_db
         self.label_dict = label_dict
 
-        # dataset specific attributes
         self.db_attributes = {
             'dataset_name': 'ego4d',
             'tiou_thresholds': np.linspace(0.1, 0.5, 5),
             'empty_label_ids': []
         }
 
+        print(f"--> Dataset loaded: {len(self.data_list)} videos found for split {self.split}")
+
     def get_attributes(self):
         return self.db_attributes
 
     def _load_json_db(self, json_file):
-        # load database and select the subset
         with open(json_file, 'r') as fid:
             json_data = json.load(fid)
         json_db = json_data['database']
 
-        # if label_dict is not available
         if self.label_dict is None:
             label_dict = {}
             for key, value in json_db.items():
-                if 'annotations' not in value:
-                    continue
+                if 'annotations' not in value: continue
                 for act in value['annotations']:
                     label_dict[act['label']] = act['label_id']
 
-        # fill in the db (immutable afterwards)
         dict_db = tuple()
         for key, value in json_db.items():
-            # skip the video if not in the split
+
             if value['subset'].lower() not in self.split:
                 continue
-            # or does not have the feature file
-            feat_files = [os.path.join(
-                folder, self.file_prefix + key + self.file_ext
-            ) for folder in self.feat_folder]
-            if not all([os.path.exists(file) for file in feat_files]):
+
+            actual_feat_path = None
+            for folder in self.feat_folder:
+                search_pattern = os.path.join(folder, f"*{key}*{self.file_ext}")
+                found_files = glob.glob(search_pattern)
+                if found_files:
+                    actual_feat_path = found_files[0]
+                    break
+            
+            if actual_feat_path is None:
                 continue
 
-            # get fps if available
-            if self.default_fps is not None:
-                fps = self.default_fps
-            elif 'fps' in value:
-                fps = value['fps']
-            else:
-                assert False, "Unknown video FPS."
+            fps = self.default_fps if self.default_fps is not None else value.get('fps', 30)
+            duration = value.get('duration', 1e8)
 
-            # get video duration if available
-            if 'duration' in value:
-                duration = value['duration']
-            else:
-                duration = 1e8
-
-            # get annotations if available
             if ('annotations' in value) and (len(value['annotations']) > 0):
                 num_acts = len(value['annotations'])
                 segments = np.zeros([num_acts, 2], dtype=np.float32)
@@ -129,45 +114,36 @@ class EGO4DDataset(Dataset):
                     segments[idx][1] = act['segment'][1]
                     labels[idx] = label_dict[act['label']]
             else:
-                segments = None
-                labels = None
+                segments, labels = None, None
 
             dict_db += ({'id': key,
+                         'actual_path': actual_feat_path, 
                          'fps' : fps,
                          'duration' : duration,
                          'segments' : segments,
                          'labels' : labels,
-                         'offset': value.get('offset'), # only for test
+                         'offset': value.get('offset'),
             }, )
 
         return dict_db, label_dict
 
-    def __len__(self):
-        return len(self.data_list)
-
     def __getitem__(self, idx):
-        # directly return a (truncated) data point (so it is very fast!)
-        # auto batching will be disabled in the subsequent dataloader
-        # instead the model will need to decide how to batch / preporcess the data
         video_item = self.data_list[idx]
 
-        # load features
-        filenames = [os.path.join(
-            folder, self.file_prefix + video_item['id'] + self.file_ext
-        ) for folder in self.feat_folder]
-        feats = np.concatenate(
-            [np.load(name).astype(np.float32) for name in filenames], axis=1
-        )
+        feats = np.load(video_item['actual_path'])
+        
+        if isinstance(feats, np.lib.npyio.NpzFile):
+            key = feats.files[0]
+            feats = feats[key]
+            
+        feats = feats.astype(np.float32)
 
-        # deal with downsampling (= increased feat stride)
         feats = feats[::self.downsample_rate, :]
         feat_stride = self.feat_stride * self.downsample_rate
         feat_offset = 0.5 * self.num_frames / feat_stride
-        # T x C -> C x T
+        
         feats = torch.from_numpy(np.ascontiguousarray(feats.transpose()))
 
-        # convert time stamp (in second) into temporal feature grids
-        # ok to have small negative values here
         if video_item['segments'] is not None:
             segments = torch.from_numpy(
                 video_item['segments'] * video_item['fps'] / feat_stride - feat_offset
@@ -176,11 +152,10 @@ class EGO4DDataset(Dataset):
         else:
             segments, labels = None, None
 
-        # return a data dict
         data_dict = {'video_id'        : video_item['id'],
-                     'feats'           : feats,      # C x T
-                     'segments'        : segments,   # N x 2
-                     'labels'          : labels,     # N
+                     'feats'           : feats,
+                     'segments'        : segments,
+                     'labels'          : labels,
                      'fps'             : video_item['fps'],
                      'duration'        : video_item['duration'],
                      'feat_stride'     : feat_stride,
@@ -188,7 +163,6 @@ class EGO4DDataset(Dataset):
                      'offset'          : video_item['offset'],
         }
 
-        # truncate the features during training
         if self.is_training and (segments is not None):
             data_dict = truncate_feats(
                 data_dict, self.max_seq_len, self.trunc_thresh, feat_offset, self.crop_ratio
